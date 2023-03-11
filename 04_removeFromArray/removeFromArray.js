3d84650771a4fb74ebe6945f1e17fb09f683edb1
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-03-11 21:35:10 +0000

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
 
+    array.forEach((item) => {
+        const i = array.indexOf(item);
+        if (!args.includes(item)) {
+            newArray.push(array[i]);
+        }
+    });
+    
+    return newArray;
 };
 
 // Do not edit below this line
