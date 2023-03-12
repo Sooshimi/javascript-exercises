157d7b2723d95c0599ade7ba65aa7ae41fd14747
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-03-12 21:43:29 +0000

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((oldestPerson,currentPerson) => {
+        let oldestAge = getAge(oldestPerson.yearOfDeath, oldestPerson.yearOfBirth);
+        let currentPersonAge = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+        return (oldestAge < currentPersonAge) ? currentPerson : oldestPerson;
+    });
 
+    function getAge(deathDate,birthDate) {
+        if (!deathDate) {
+            deathDate = new Date().getFullYear();
+        }
+        return deathDate - birthDate;
+    }
 };
 
 // Do not edit below this line
