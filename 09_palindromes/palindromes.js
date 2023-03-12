516c94219f694b867d5cbe8433de101e4423d237
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-03-12 15:48:14 +0000

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z]/g, "");
+    let backwards = string.split("").reverse("").join("").replace(/[^a-z]/g, "");
+    if (backwards === string) return true;
+    else return false;
 };
+
+console.log(palindromes('racecar!'));
 
 // Do not edit below this line
 module.exports = palindromes;
