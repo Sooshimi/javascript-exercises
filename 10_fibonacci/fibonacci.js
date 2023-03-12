29f2ae0be020556ee0b1d057770b516413372c8b
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-03-12 20:51:50 +0000

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const count = Number(num);
+    if (count < 0 ) return "OOPS";
+    let a = 0; // prev term in current seq
+    let b = 1; // term in current seq
+    for (let i = 1; i < count; i++) {
+        let ahead = b;
+        b = a + b;
+        a = ahead;
+    }
+    return b;
 };
 
 // Do not edit below this line
