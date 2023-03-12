1c1e0ac081f5e7f6bb90fa97790c164307676ffe
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-03-12 23:03:03 +0000

    Partially complete exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,17 @@
-const caesar = function() {
+const caesar = function (string, shift) {
+    let array = string.split("");
+    let newArray = array.map((char) => shiftChar(char, shift));
+    return newArray.join("")
+};
 
+function shiftChar(char, shift) {
+    const charCode = char.charCodeAt();
+    if ((charCode >= 65 && charCode <= 90) ||
+        (charCode >= 97 && charCode <= 122)) {
+        let shiftedChar = charCode + shift;
+        return String.fromCharCode(shiftedChar);
+    }
+    return char;
 };
 
 // Do not edit below this line
